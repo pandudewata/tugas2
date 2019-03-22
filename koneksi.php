4f976be2c50a11e04abc5d83655f569a00d9ec75
pandudewata <48473582+pandudewata@users.noreply.github.com>
2019-03-22 16:57:15 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost","id7899369_hikam","12345","id7899369_akademik");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
